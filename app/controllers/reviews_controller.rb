fb9862b134a4e5f5055515b064e4626eeb8d1b0c
Garry Gabin <gabingarry972@gmail.com>
2022-11-15 15:21:14 -0400

    okok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,20 @@
 class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
-<<<<<<< HEAD
-    @quad = List.find(params[:quad_id])
-=======
-    @list = List.find(params[:list_id])
->>>>>>> fe8f61f81dcbbc8800ce7a19d728e52aff48f901
-    @review.list = @list
+    @quad = quad.find(params[:quad_id])
+    @review.quad = @quad
     if @review.save
-      redirect_to list_path(@list)
+      redirect_to quad_path(@quad)
     else
-<<<<<<< HEAD
       @booking = Booking.new
       render 'quads/show', status: :unprocessable_entity
-=======
-      @bookmark = Bookmark.new
-      render 'lists/show', status: :unprocessable_entity
->>>>>>> fe8f61f81dcbbc8800ce7a19d728e52aff48f901
     end
   end
 
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
-    redirect_to list_path(@review.list)
+    redirect_to quad_path(@review.quad)
   end
 
   private
