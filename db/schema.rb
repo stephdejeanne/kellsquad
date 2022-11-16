ec29afe07f504694f2c549a15c1c4dc25316c610
Stephane Chaigneau <stephanechaigneau@gmail.com>
2022-11-16 10:44:03 -0400

    ajout des photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_181539) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_140245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_181539) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+    t.boolean "free"
+    t.string "img"
   end
 
   create_table "reviews", force: :cascade do |t|
