e45cafc6fa5d41b251bbc35d3da3791626b24e9d
Kelly Nerestan <jck.martinique@yahoo.com>
2022-11-16 11:12:50 -0400

    add column migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_140245) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_145807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_140245) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start"
+    t.datetime "end"
     t.index ["quad_id"], name: "index_bookings_on_quad_id"
     t.index ["review_id"], name: "index_bookings_on_review_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
