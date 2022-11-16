08368412ddbc3843793e1b2304c75c8a9c65c88a
Stephane Chaigneau <stephanechaigneau@gmail.com>
2022-11-16 13:55:32 -0400

    divers-finition

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,9 +9,16 @@ class BookingsController < ApplicationController
     redirect_to booking_path(@booking)
   end
 
+  def show
+  end
+
   private
 
   def restaurant_params
     params.require(:booking).permit(:user_id, :quad_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
