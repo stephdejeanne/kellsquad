67c210abecfeabbd06697617138fe5a3cbfffd52
Kelly Nerestan <jck.martinique@yahoo.com>
2022-11-16 14:42:59 -0400

    ajout reservation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,15 +3,33 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
   end
 
+  def new
+    @quad = Quad.find(params[:quad_id])
+    @booking = Booking.new
+  end
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def create
-    @booking = Booking.new(params[:booking])
-    @booking.save
-    redirect_to booking_path(@booking)
+    @quad = Quad.find(params[:quad_id])
+    arg = booking_params
+    arg[:user_id] = current_user.id
+    arg[:quad_id] = params[:quad_id]
+    @booking = Booking.new(arg)
+    if @booking.save!
+      redirect_to quad_bookings(@booking)
+    else
+      render :new
+    end
   end
 
   private
+  def set_quad
+    @quad = Quad.find(params[:quad_id])
+  end
 
-  def restaurant_params
-    params.require(:booking).permit(:user_id, :quad_id)
+  def booking_params
+    params.require(:booking).permit(:start, :end)
   end
 end
