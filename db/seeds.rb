d82640f44e980d86f663c9b92b83a67712eb562e
Stephane Chaigneau <stephanechaigneau@gmail.com>
2022-11-17 10:32:20 -0400

    APasGarder

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.destroy_all
 
 p 'Creating quad'
 quads = Quad.create!([{ name: "Terminator", color: "black", power: 50, price: 150, free: true, img: "https://images.unsplash.com/photo-1549221279-aca391fb56d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cXVhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60", address: "Rue de la République, 97200 Fort-de-France" },
-  { name: "Classic", color: "blue", power: 45, price: 160 , free: true, img: "https://images.unsplash.com/photo-1553786815-138920395604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHF1YWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60", address: "Habitation Petit Morne, 97232 Le Lamentin" },
+  { name: "Classic", color: "blue", power: 45, price: 160 , free: true, img: "https://images.unsplash.com/photo-1503438792824-0882f145c89d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cXVhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60", address: "Habitation Petit Morne, 97232 Le Lamentin" },
   { name: "Woman's friendly", color: "blue", power: 40, price: 170, free: true, img: "https://images.unsplash.com/photo-1516933857371-5a895d657803?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fHF1YWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60", address: "Habitation Petit Morne, 97232 Le Lamentin" },
   { name: "For the kid", color: "orange", power: 35, price: 180, free: true, img:"https://images.unsplash.com/photo-1532934133442-e9cb961676f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHF1YWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60", address: "Rue de la République, 97200 Fort-de-France" },
 
